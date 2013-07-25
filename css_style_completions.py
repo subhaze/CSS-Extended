12dc75147b131618e55cf9145fba055c09a4df6e
Michael Russell <jmrussell22@gmail.com>
2013-07-24 21:40:35 -0400

    allows for spaces when defining classes

--- css_style_completions.py
+++ css_style_completions.py
@@ -3,13 +3,16 @@ import sublime, sublime_plugin
 class CssStyleCompletions(sublime_plugin.EventListener):
 	def on_query_completions(self, view, prefix, locations):
 		selector = view.match_selector(locations[0], 'text.html string')
-		attribute = view.substr(view.expand_by_class(locations[0], sublime.CLASS_WORD_START | sublime.CLASS_WORD_END, ' >'))
-		if selector and attribute.startswith(('class', 'ng-class')):
+		attribute = view.expand_by_class(locations[0], sublime.CLASS_PUNCTUATION_START | sublime.CLASS_PUNCTUATION_END, '"\'')
+		start = attribute.begin() - 7
+		end = attribute.end()
+		attribute = view.substr(sublime.Region(start, end))
+		if selector and attribute.startswith('class'):
 			results = []
 			for _view in sublime.active_window().views():
 				results += [(_view.substr(point).replace('.','').replace('#','') + "\t CSS", _view.substr(point).replace('.','').replace('#','')) for point in _view.find_by_selector('entity.other.attribute-name')]
 			results = list(set(results))
-			return results
+			return (results, 0)
 		if selector and attribute.startswith(('style')):
 			print(prefix, 'prefix')
 			return [
