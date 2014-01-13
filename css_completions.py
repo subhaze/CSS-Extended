a12666897311e74dd15f2670c87cbee17f8cb8b7
subhaze <jmrussell22@gmail.com>
2014-01-13 09:51:21 -0500

    Adds moar CSS prop/values to completion list

--- css_completions.py
+++ css_completions.py
@@ -111,6 +111,83 @@ css_data = """
 "border-width"=<border-width>
 "box-decoration-break"=slice | clone
 "box-shadow"=none | <shadow> | none
+"box-sizing"=border-box | padding-box
+
+
+
+"icon"=auto | <uri> | inherit
+"box-sizing"=content-box | padding-box | border-box | inherit
+"outline"=<outline-color> | <outline-style> | <outline-width> | inherit
+"outline-width"=<border-width> | inherit
+"outline-style"=auto | <border-style> | inherit
+"outline-color"=<color> | invert | inherit
+"outline-offset"=<length> | inherit
+"resize"=none | both | horizontal | vertical | inherit
+"text-overflow"=clip | ellipsis | inherit
+"cursor"=<uri> | auto | default | none | context-menu | help | pointer | progress | wait | cell | crosshair | text | vertical-text | alias | copy | move | no-drop | not-allowed | e-resize | n-resize | ne-resize | nw-resize | s-resize | se-resize | sw-resize | w-resize | ew-resize | ns-resize | nesw-resize | nwse-resize | col-resize | row-resize | all-scroll | zoom-in | zoom-out | inherit
+"nav-index"=auto | <number> | inherit
+"nav-up" "nav-right" "nav-down" "nav-left"=auto | current | root | inherit
+"ime-mode"=auto | normal | active | inactive | disabled | inherit
+
+
+
+"transform"=none | <transform-list>
+"transform-origin"=left | center | right | top | bottom | <percentage> | <length>
+"transform-style"=flat | preserve-3d
+"perspective"=none | <length>
+"perspective-origin"=left | center | right | top | bottom | <percentage> | <length>
+"backface-visibility"=visible | hidden
+
+"text-transform"=none | capitalize | uppercase | lowercase | full-width
+"white-space"=normal | pre | nowrap | pre-wrap | pre-line
+"tab-size"=<integer> | <length>
+"line-break"=auto | loose | normal | strict
+"word-break"=normal | keep-all | break-all
+"hyphens"=none | manual | auto
+"overflow-wrap"=normal | break-word
+"word-wrap"=normal | break-word
+"text-align"=start | end | left | right | center | justify | match-parent | start end
+"text-align-last"=auto | start | end | left | right | center | justify
+"text-justify"=auto | none | inter-word | distribute
+"word-spacing"=normal | <length> | <percentage>
+"letter-spacing"=normal | <length>
+"text-indent"=<length> | <percentage> | hanging | each-line
+"hanging-punctuation"=none | first | force-end | allow-end | last
+
+"transition-property"=none | <single-transition-property>
+"transition-duration"=<time>
+"transition-timing-function"=<single-transition-timing-function>
+"transition-delay"=<time>
+"transition"=<single-transition>
+
+"animation-name"=<single-animation-name>
+"animation-duration"=<time>
+"animation-timing-function"=<single-timing-function>
+"animation-iteration-count"=<single-animation-iteration-count>
+"animation-direction"=<single-animation-direction>
+"animation-play-state"=<single-animation-play-state>
+"animation-delay"=<time>
+"animation-fill-mode"=<single-animation-fill-mode>
+"animation"=<single-animation>
+
+"font-family"=<family-name> | <generic-family>
+"font-weight"=normal | bold | bolder | lighter | 100 | 200 | 300 | 400 | 500 | 600 | 700 | 800 | 900
+"font-stretch"=normal | ultra-condensed | extra-condensed | condensed | semi-condensed | semi-expanded | expanded | extra-expanded | ultra-expanded
+"font-style"=normal | italic | oblique
+"font-size"=<absolute-size> | <relative-size> | <length> | <percentage>
+"font-size-adjust"=none | <number>
+"font"=<font-style> | <font-variant-css21> | <font-weight> | <font-stretch> | <font-size> | <line-height> | <font-family> | caption | icon | menu | message-box | small-caption | status-bar
+"font-synthesis"=none | weight | style
+"font-kerning"=auto | normal | none
+"font-variant-ligatures"=normal | none | <common-lig-values> | <discretionary-lig-values> | <historical-lig-values> | <contextual-alt-values>
+"font-variant-position"=normal | sub | super
+"font-variant-caps"=normal | small-caps | all-small-caps | petite-caps | all-petite-caps | unicase | titling-caps
+"font-variant-numeric"=normal | <numeric-figure-values> | <numeric-spacing-values> | <numeric-fraction-values> | ordinal | slashed-zero
+"font-variant-alternates"=normal | stylistic(<feature-value-name>) | historical-forms | styleset(<feature-value-name>) | character-variant(<feature-value-name>) | swash(<feature-value-name>) | ornaments(<feature-value-name>) | annotation(<feature-value-name>)
+"font-variant-east-asian"=normal | <east-asian-variant-values> | <east-asian-width-values> | ruby
+"font-variant"=normal | none | <common-lig-values> | <discretionary-lig-values> | <historical-lig-values> | <contextual-alt-values> | stylistic(<feature-value-name>) | historical-forms | styleset(<feature-value-name>) | character-variant(<feature-value-name>) | swash(<feature-value-name>) | ornaments(<feature-value-name>) | annotation(<feature-value-name>) | small-caps | all-small-caps | petite-caps | all-petite-caps | unicase | titling-caps | <numeric-figure-values> | <numeric-spacing-values> | <numeric-fraction-values> | ordinal | slashed-zero | <east-asian-variant-values> | <east-asian-width-values> | ruby
+"font-feature-settings"=normal | <feature-tag-value>
+"font-language-override"=normal | <string>
 """
 
 def parse_css_data(data):
